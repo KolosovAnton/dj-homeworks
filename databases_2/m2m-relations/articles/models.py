d122601ef5a32beb885687fc3f8989ee2af3feb1
Anton Kolosov <antonio08.02.88@gmail.com>
2019-10-25 20:55:45 +0300

    solution

--- databases_2/m2m-relations/articles/models.py
+++ databases_2/m2m-relations/articles/models.py
@@ -7,6 +7,7 @@ class Article(models.Model):
     text = models.TextField(verbose_name='Текст')
     published_at = models.DateTimeField(verbose_name='Дата публикации')
     image = models.ImageField(null=True, blank=True, verbose_name='Изображение',)
+    articles = models.ManyToManyField('Scope', through='ArticleScope')
 
     class Meta:
         verbose_name = 'Статья'
@@ -14,3 +15,27 @@ class Article(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Scope(models.Model):
+    topic = models.CharField(max_length=128, verbose_name='Наименование тэга')
+
+    class Meta:
+        verbose_name = 'Тэг'
+        verbose_name_plural = 'Тэги'
+
+    def __str__(self):
+        return self.topic
+
+
+class ArticleScope(models.Model):
+    topic = models.ForeignKey(Scope, on_delete=models.CASCADE, verbose_name='Категория', related_name='scopes')
+    article = models.ForeignKey(Article, on_delete=models.CASCADE, related_name='scopes')
+    is_main = models.BooleanField(verbose_name='Основной тэг')
+
+    class Meta:
+        verbose_name = 'Тематика тэга'
+        verbose_name_plural = 'Тематики тэга'
+
+    def __str__(self):
+        return f'{self.article}_{self.topic}'
